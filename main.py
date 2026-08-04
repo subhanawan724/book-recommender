a62bc3b2075923682bb866196a5b66a4e89d71e3
subhanawan724 <subhanawan630@gmail.com>
2026-08-04 08:09:44 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import pandas as pd
 from langchain_chroma import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
+
 
 BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "data"
